67dd7ba2094054802093200af0d9db25d5b37d53
Matt Long <nietscape@gmail.com>
2024-11-26 13:02:50 -0800

    put server creation into a function

--- src/stub-server/server.ts
+++ src/stub-server/server.ts
@@ -2,33 +2,33 @@ import jsonServer from "json-server";
 
 import { seedBoards } from "./seed";
 
-const server = jsonServer.create();
-const router = jsonServer.router("src/stub-server/db.json");
-const middlewares = jsonServer.defaults();
+export const createJsonServer = () => {
+  const server = jsonServer.create();
+  const router = jsonServer.router("src/stub-server/db.json");
+  const middlewares = jsonServer.defaults();
 
-server.use((req, res, next) => {
-  if (req.method === "DELETE" && req.query["_cleanup"]) {
-    const db = router.db;
-    db.set("boards", []).write();
-    res.sendStatus(204);
-  } else {
-    next();
-  }
-});
+  server.use((req, res, next) => {
+    if (req.method === "DELETE" && req.query["_cleanup"]) {
+      const db = router.db;
+      db.set("boards", []).write();
+      res.sendStatus(204);
+    } else {
+      next();
+    }
+  });
 
-server.use((req, res, next) => {
-  if (req.method === "GET" && req.query["_seed"]) {
-    const db = router.db;
-    db.set("boards", seedBoards(100)).write();
-    res.sendStatus(201);
-  } else {
-    next();
-  }
-});
+  server.use((req, res, next) => {
+    if (req.method === "GET" && req.query["_seed"]) {
+      const db = router.db;
+      db.set("boards", seedBoards(100)).write();
+      res.sendStatus(201);
+    } else {
+      next();
+    }
+  });
 
-server.use(middlewares);
-server.use(router);
+  server.use(middlewares);
+  server.use(router);
 
-server.listen(8080, () => {
-  console.log("JSON server is running");
-});
+  return server;
+};
