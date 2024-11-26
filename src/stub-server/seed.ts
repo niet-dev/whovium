907d92f6391b14f7f013dd5af44d437eb9d5bcc0
Matt Long <nietscape@gmail.com>
2024-11-25 16:28:23 -0800

    stub server seed and cleanup middleware

---
+++ src/stub-server/seed.ts
@@ -0,0 +1,18 @@
+import { faker } from "@faker-js/faker";
+
+import type { Board } from "@/lib/types";
+
+export const seedBoards = (numBoards: number): Board[] => {
+  faker.seed(24);
+  const boardList: Board[] = [];
+  for (let i = 0; i < numBoards; i++) {
+    boardList.push({
+      id: faker.string.uuid(),
+      title: faker.book.title(),
+      createdBy: faker.book.author(),
+      description: faker.lorem.sentences(2),
+    });
+  }
+  faker.seed();
+  return boardList;
+};
