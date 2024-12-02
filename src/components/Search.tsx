f7f3e583d08e4250e8c8c8791d449d6ae14986e5
Matt Long <nietscape@gmail.com>
2024-12-02 12:06:14 -0800

    Search has styled outline when focused

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -27,7 +27,7 @@ const Search = ({ placeholder }: { placeholder: string }) => {
         placeholder={placeholder}
         onChange={(e) => handleSearch(e.target.value)}
         defaultValue={searchParams.get("query")?.toString()}
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm focus:outline focus:outline-pink-500 placeholder:text-gray-500"
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
