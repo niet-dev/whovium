7d4a7f586d811970b4e2eb9a9df546b2a67604f1
Matt Long <nietscape@gmail.com>
2024-12-02 08:36:26 -0800

    styles for search bar

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -20,14 +20,15 @@ const Search = ({ placeholder }: { placeholder: string }) => {
   };
 
   return (
-    <div role="search">
+    <div role="search" className="relative flex flex-1 flex-shrink-0">
       <input
         type="search"
         placeholder={placeholder}
         onChange={(e) => handleSearch(e.target.value)}
         defaultValue={searchParams.get("query")?.toString()}
+        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
       />
-      <MagnifyingGlassIcon />
+      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
   );
 };
