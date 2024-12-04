19f579a5bf70a135165c938515b948e858f65213
Matt Long <nietscape@gmail.com>
2024-12-03 17:39:13 -0800

    initial seed configuration

---
+++ src/prisma/seed.ts
@@ -0,0 +1,66 @@
+import { faker } from "@faker-js/faker";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const fakerSeed = 24;
+
+const seedUsers = async (numUsers: number) => {
+  for (let i = 0; i < numUsers; i++) {
+    await prisma.user.create({
+      data: {
+        username: faker.internet.username(),
+      },
+    });
+  }
+};
+
+const seedBoards = async (numUsers: number, numBoards: number) => {
+  for (let i = 0; i < numBoards; i++) {
+    await prisma.board.create({
+      data: {
+        title: faker.book.series(),
+        imgSrc: "https://placehold.co/300x300",
+        description: faker.lorem.paragraph(),
+        userId: Math.floor(Math.random() * numUsers + 1),
+      },
+    });
+  }
+};
+
+const seedCards = async (numBoards: number, numCards: number) => {
+  for (let i = 0; i < numCards; i++) {
+    await prisma.card.create({
+      data: {
+        name: faker.person.fullName(),
+        imgSrc: "https://placehold.co/100x100",
+        boardId: Math.floor(Math.random() * numBoards + 1),
+      },
+    });
+  }
+};
+
+const seedDb = async ({
+  numUsers,
+  numBoards,
+  numCards,
+}: {
+  numUsers: number;
+  numBoards: number;
+  numCards: number;
+}) => {
+  faker.seed(fakerSeed);
+  await seedUsers(numUsers);
+  await seedBoards(numUsers, numBoards);
+  await seedCards(numBoards, numCards);
+  faker.seed();
+};
+
+seedDb({ numUsers: 20, numBoards: 100, numCards: 500 })
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
