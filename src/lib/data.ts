b4ebdc5540d1de939af2c5b64c122f945400ff66
Matt Long <nietscape@gmail.com>
2024-12-05 14:39:25 -0800

    rename actions.ts to data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,8 +1,10 @@
 import prisma from "./prisma";
 
+const ITEMS_PER_PAGE = 10;
+
 export const fetchBoardList = async (query: string) => {
   const res = await prisma.board.findMany({
-    take: 10,
+    take: ITEMS_PER_PAGE,
     where: {
       title: {
         contains: query,
@@ -17,4 +19,18 @@ export const fetchBoardList = async (query: string) => {
     },
   });
   return res;
+};
+
+export const fetchBoardPages = async (query: string) => {
+  const res = await prisma.board.count({
+    where: {
+      title: {
+        contains: query,
+        mode: "insensitive",
+      },
+    },
+  });
+
+  const pageCount = Math.ceil(res / ITEMS_PER_PAGE);
+  return pageCount;
 };
