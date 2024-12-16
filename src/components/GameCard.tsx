0e3341f7a8ac38cf167be384138227e96d3fe214
Matt Long <nietscape@gmail.com>
2024-12-15 17:12:53 -0800

    implement basic card selection animation

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,19 +1,42 @@
 "use client";
 
+import { motion, useAnimate } from "motion/react";
 import Image from "next/image";
 import type { Card as GameCardType } from "@prisma/client";
+import { useState } from "react";
 
 const GameCard = ({ card }: { card: GameCardType }) => {
+  const [isSelected, setIsSelected] = useState(false);
+  const [scope, animate] = useAnimate();
+
+  const handleClick = () => {
+    if (!isSelected) {
+      animate(scope.current, { opacity: 0.7 }, { duration: 0.3 });
+    } else {
+      animate(scope.current, { opacity: 0 }, { duration: 0.3 });
+    }
+
+    setIsSelected(!isSelected);
+  };
+
   return (
     <div className="flex flex-col justify-center gap-2">
-      <div className="relative h-[350px] w-[250px] rounded-lg shadow-inner">
+      <motion.div
+        onClick={handleClick}
+        className="relative h-[350px] w-[250px] rounded-lg shadow-inner"
+        whileHover={{ y: -5 }}
+      >
         <Image
           src={card.imgSrc}
           alt={`Image for card ${card.name}`}
           fill
-          className="-z-10 rounded-lg"
+          className="-z-20 rounded-lg"
         />
-      </div>
+        <div
+          ref={scope}
+          className="bg-red-800 opacity-0 h-full rounded-lg -z-10"
+        />
+      </motion.div>
       <div className="text-center py-1">
         <h2 className="">{card.name}</h2>
       </div>
