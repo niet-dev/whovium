f0096c7c04ce8978bdbdf49dfbae6b4537e4e311
Matt Long <nietscape@gmail.com>
2025-01-11 13:11:02 -0800

    apply route protection via middleware

--- src/auth.ts
+++ src/auth.ts
@@ -3,4 +3,10 @@ import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
+  callbacks: {
+    authorized: async ({ auth }) => {
+      // Logged in users are authenticated, otherwise redirect to login page
+      return !!auth;
+    },
+  },
 });
