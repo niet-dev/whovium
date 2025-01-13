c75cb65db65f40a7e07cdc6cc2d2d66c647d154e
Matt Long <nietscape@gmail.com>
2025-01-13 13:57:30 -0800

    add basic styles to sign in page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,9 +2,9 @@ import SignInGithubButton from "@/components/SignInGithubButton";
 
 export default function Page() {
   return (
-    <>
-      <h1>Sign in</h1>
+    <div className="container mx-auto mt-[40vh] flex max-w-sm flex-col space-y-5 rounded-md border p-4">
+      <h1 className="text-center text-lg font-bold">Sign in</h1>
       <SignInGithubButton />
-    </>
+    </div>
   );
 }
