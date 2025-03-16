7c3ec41b6c7d4bdd0995de35f6de52e8a46490a5
Matt Long <nietscape@gmail.com>
2025-03-15 17:05:20 -0700

    use pooled database connection

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,7 +5,7 @@ import ws from "ws";
 
 neonConfig.webSocketConstructor = ws;
 
-const connectionString = `${process.env.DATABASE_URL_UNPOOLED}`;
+const connectionString = `${process.env.DATABASE_URL}`;
 
 const pool = new Pool({ connectionString });
 const adapter = new PrismaNeon(pool);
