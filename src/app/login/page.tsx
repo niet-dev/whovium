80ae081aa47981f0d5f84a1e2a6b2c353565d02f
Matt Long <nietscape@gmail.com>
2025-04-07 20:45:23 -0700

    restyle sign in page to use brand colors

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,15 +2,11 @@ import SignInGithubButton from "@/components/sign-in-github-button";
 
 export default function Page() {
   return (
-    <div className="container mx-auto mt-[30vh] flex justify-center">
-      <div className="flex max-w-sm flex-col rounded-md border p-6 sm:p-10">
-        <div className="mb-8 text-center">
-          <h1 className="my-3 text-4xl font-bold">Sign in</h1>
-          <p className="text-sm">Sign in to access your account</p>
-        </div>
-        <div className="my-4 space-y-4">
-          <SignInGithubButton />
-        </div>
+    <div className="relative container mx-auto flex justify-center px-4 py-42">
+      <div className="bg-fill inset-ring-stroke-weak flex flex-col space-y-4 rounded-md p-8 text-center inset-ring-2">
+        <h1 className="text-text-strong text-3xl font-bold">Sign in</h1>
+        <p className="text-text-weak text-sm">Sign in to access your account</p>
+        <SignInGithubButton />
       </div>
     </div>
   );
