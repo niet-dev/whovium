f15ea0a7d423c0a4189b8230692ffeb48bf8b6f5
Matt Long <nietscape@gmail.com>
2025-04-30 12:44:38 -0700

    tweak background to cover the entire page on large screens

--- src/components/colored-background.tsx
+++ src/components/colored-background.tsx
@@ -10,7 +10,7 @@ export default function ColoredBackground({
       <div className="from-brand to-background/0 absolute inset-0 bg-linear-to-r from-0% to-15%"></div>
       <div className="to-brand from-background/0 absolute inset-0 bg-linear-to-r from-85%"></div>
 
-      <div className="bg-brand h-full min-h-[95dvh] w-full bg-[url('/question-mark-bg.svg')] bg-repeat">
+      <div className="bg-brand h-full min-h-screen w-full bg-[url('/question-mark-bg.svg')] bg-repeat">
         {children}
       </div>
     </div>
--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -22,7 +22,7 @@ type NavBarProps = {
 
 export default function NavBar({ user }: NavBarProps) {
   return (
-    <header className="bg-background">
+    <header className="bg-background relative">
       <nav className="container mx-auto flex h-14 w-full justify-between p-4">
         <Link href="/" className="text-brand flex items-center font-bold">
           W?
